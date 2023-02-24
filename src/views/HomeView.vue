1287100dcbe3721790acc28d0690b7d4e511ccc5
Babatunde Owolabi <chatwittundeowolabi@gmail.com>
2023-02-24 19:22:13 +0100

    refs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="home">
-
+    <h1>Home</h1>
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
+    <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name">
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
-  name: 'HomeView'
+  name: 'Home',
+  setup() {
+    const name = ref('mario')
+    const age = ref(30)
+
+    // const p = ref(null)
+
+    const handleClick = (e) => {
+      name.value = 'luigi'
+      age.value = 35
+    }
+
+    return { name, age, handleClick }
+  },
 }
 </script>
